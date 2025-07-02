19dba66ef3ff579633df4def0b5610ad641098bf
Tanishq Lokhande <tanishq.lokhande@newvisionsoftpune.com>
2025-07-02 13:03:37 +0530

    Signals in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 import { Signup } from "./signup/signup";
